ab9ddd998a7003570d45802b06b49e0ec00d9065
TRJackson998 <t.r.jackson998@gmail.com>
2023-10-08 12:15:18 -0700

    Fulfill Requirement: accept floating pt literals modify the literal token so that it accepts unsigned floating point literals.

--- operand.cpp
+++ operand.cpp
@@ -1,12 +1,22 @@
-// CMSC 330 Advanced Programming Languages
-// Project 2 Skeleton
-// UMGC CITE
-// Spring 2023
+/*
+Jackson, Terrence
+CMSC 330 Project 2
+10.08.2023
 
-// This file contains the body of the function parseName contained in the Operand class. That function
-// parses the next token. When the next non-whitespace character is a digit, it assumes a literal is next.
-// When the next character is a left parenthesis, a recursive call is made to parse the subexpression.
-// Otherwise the next token is assumed to a variable. No checks are made to ensure correct syntax.
+CMSC 330 Advanced Programming Languages
+Project 2 Skeleton
+UMGC CITE
+Spring 2023
+
+This file contains the body of the function parseName contained in the Operand class. That function
+parses the next token. When the next non-whitespace character is a digit, it assumes a literal is next.
+When the next character is a left parenthesis, a recursive call is made to parse the subexpression.
+Otherwise the next token is assumed to a variable. No checks are made to ensure correct syntax.
+
+Additions:
+    change value variable from int to double, so that floating point values will be read in
+    correctly from the input stream
+*/
 
 #include <cctype>
 #include <iostream>
@@ -25,7 +35,7 @@ using namespace std;
 Expression *Operand::parse(stringstream &in)
 {
     char paren;
-    int value;
+    double value;
 
     in >> ws;
     if (isdigit(in.peek()))
